faa5f8af1a1ac765fdc9e42a815f023ca3f7b340
KatoMono Enkeli <k4t0mono@gmail.com>
2017-11-21 18:43:15 -0200

    UsuarioDAO adicionado

---
+++ src/pongsys/persistence/UsuarioDAO.java
@@ -0,0 +1,37 @@
+package pongsys.persistence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pongsys.model.Usuario;
+
+public class UsuarioDAO {
+    
+//  Lista de usuarios cadastrados
+    private final List<Usuario> usuarios;
+    
+//  Única instância da classe
+    private static UsuarioDAO instancia;
+    
+    private UsuarioDAO() {
+        this.usuarios = new ArrayList();
+    }
+
+    public static UsuarioDAO getInstancia() {
+        return instancia;
+    }
+    
+    public Usuario getUsuario(String email) {
+        for(Usuario u : this.usuarios) {
+            if(email.equals(u.getEmail())) {
+                return u;
+            }
+        }
+        
+        return null;
+    }
+    
+    public void adicionarUsuario(Usuario u) {
+        this.usuarios.add(u);
+    }
+}
