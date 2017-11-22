c3e496b224255e40a28e25f02d11f29011ab97b4
silventino <silventinogato@gmail.com>
2017-11-22 18:25:52 -0200

    layout criado

--- src/br/ufla/dcc/ppoo/view/TelaPrincipal.java
+++ src/br/ufla/dcc/ppoo/view/TelaPrincipal.java
@@ -1,32 +1,48 @@
 package br.ufla.dcc.ppoo.view;
 
-import java.awt.Button;
+
 import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 public class TelaPrincipal extends JFrame {
     
     private GridBagLayout gbl;
     private GridBagConstraints gbc;
+    JButton jb;
+    JButton jb2;
+    JButton jb3;
+    JTextArea txt;
+    
     
     public TelaPrincipal() {
         super("BachSys");
+        
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         
         gbl = new GridBagLayout();
         gbc = new GridBagConstraints();
         
+        setLayout(gbl);
         
-        
-        JButton jb = new JButton("arroz");
-        adicionarComponente(jb, GridBagConstraints.WEST, GridBagConstraints.NONE, 0, 0, 1, 1);
-        JButton jb2 = new JButton("p");
-        adicionarComponente(jb2, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, 1, 0, 0,0);
+        this.construirTela();
+     
+    }
+    
+    public void construirTela(){
+           
+        jb = new JButton("arroz");
+        adicionarComponente(jb, GridBagConstraints.CENTER, GridBagConstraints.NONE, 0, 0, 1, 1);
+        jb2 = new JButton("p");
+        adicionarComponente(jb2, GridBagConstraints.CENTER, GridBagConstraints.NONE, 1, 0, 1 ,1);
+        jb3 = new JButton("KKK");
+        adicionarComponente(jb3, GridBagConstraints.CENTER, GridBagConstraints.NONE, 0, 1, 1 ,1);
     }
     
     private void adicionarComponente(Component comp, int anchor, int fill,
@@ -40,7 +56,7 @@ public class TelaPrincipal extends JFrame {
         gbc.gridheight = alt;
         gbc.insets = new Insets(3, 3, 3, 3);
         gbl.setConstraints(comp, gbc);
-        this.add(comp);
+        add(comp);
     }
     
 }
