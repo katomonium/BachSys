2657894f1bbb292721fbe4108245d13f0bd6b438
Slavex008 <arthucruz@hotmail.com>
2017-11-27 16:21:44 -0200

    Criação da classe TelaPrincipal, tela que se abre após o login

---
+++ src/br/ufla/dcc/ppoo/view/TelaPrincipal.java
@@ -0,0 +1,22 @@
+package br.ufla.dcc.ppoo.view;
+
+
+public class TelaPrincipal extends Tela {
+
+    
+    public TelaPrincipal() {
+        super("BachSys", 800, 600);
+        construirTela();
+    }
+    
+    @Override
+    protected void construirTela() {
+        
+    }
+
+    @Override
+    protected void adicionarAcoes() {
+        
+    }
+    
+}
