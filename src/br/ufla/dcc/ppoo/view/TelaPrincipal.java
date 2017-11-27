1e7be4f04d1b7a67df079e8e6bfcc12a6fcb7c19
Slavex008 <arthucruz@hotmail.com>
2017-11-27 16:25:06 -0200

    Alteração no construtor de TelaPrincipal

--- src/br/ufla/dcc/ppoo/view/TelaPrincipal.java
+++ src/br/ufla/dcc/ppoo/view/TelaPrincipal.java
@@ -1,12 +1,21 @@
 package br.ufla.dcc.ppoo.view;
 
+import javax.swing.JPanel;
+
 
 public class TelaPrincipal extends Tela {
-
+    
+    private JPanel painelDadosUsuario;
+    private JPanel painelAcoes;
+    private JPanel painelListaMusica;
     
     public TelaPrincipal() {
         super("BachSys", 800, 600);
         construirTela();
+        
+        painelAcoes = new JPanel();
+        painelDadosUsuario = new JPanel();
+        painelListaMusica = new JPanel();
     }
     
     @Override
