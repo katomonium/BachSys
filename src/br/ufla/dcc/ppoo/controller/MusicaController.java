402d40a25e88ec7e0e90059498aa412fd2ad72e5
KatoMono Enkeli <k4t0mono@gmail.com>
2017-12-06 12:47:28 -0200

    editarMusica() adicionado

--- src/br/ufla/dcc/ppoo/controller/MusicaController.java
+++ src/br/ufla/dcc/ppoo/controller/MusicaController.java
@@ -1,9 +1,7 @@
 package br.ufla.dcc.ppoo.controller;
 
 import br.ufla.dcc.ppoo.model.Musica;
-import br.ufla.dcc.ppoo.model.Usuario;
 import br.ufla.dcc.ppoo.persistence.MusicaDAO;
-import java.util.Arrays;
 import java.util.List;
 
 public class MusicaController {
@@ -12,13 +10,11 @@ public class MusicaController {
     private static final MusicaController INSTANCIA = new MusicaController();
     
     public void addMusica(String nome, String autor, String album,
-            Integer ano, String genero, String usuario, String[] tags) {
+            int ano, String genero, String usuario, String[] tags) {
         
         MUSICA_DAO.addMusica(
                 new Musica(nome, autor, album, ano, genero, usuario, tags), usuario
         );
-        
-        System.out.println("MusicaController" + Arrays.toString(tags));
     }
 
     public static MusicaController getInstancia() {
@@ -39,6 +35,14 @@ public class MusicaController {
     
     public List<Musica> getMusicas() {
         return MUSICA_DAO.getMusicas();
+    }
+    
+    public void modificarMusica(String nome, String autor, String album,
+            int ano, String genero, String usuario, String[] tags) {
+        
+        MUSICA_DAO.editarMusica(
+            new Musica(nome, autor, album, ano, genero, usuario, tags), usuario
+        );
     }
     
 }
